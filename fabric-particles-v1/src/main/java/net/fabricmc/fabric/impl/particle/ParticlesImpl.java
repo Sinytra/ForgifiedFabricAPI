4271257c95674d36f5be096fcf452ab389a712bf
Su5eD <su5ed@outlook.com>
2023-06-28 11:16:13 +0200

    Port fabric-particles-v1

---
+++ fabric-particles-v1/src/main/java/net/fabricmc/fabric/impl/particle/ParticlesImpl.java
@@ -0,0 +1,13 @@
+package net.fabricmc.fabric.impl.particle;
+
+import net.minecraftforge.fml.IExtensionPoint;
+import net.minecraftforge.fml.ModLoadingContext;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.network.NetworkConstants;
+
+@Mod("fabric_particles_v1")
+public class ParticlesImpl {
+    public ParticlesImpl() {
+        ModLoadingContext.get().registerExtensionPoint(IExtensionPoint.DisplayTest.class, () -> new IExtensionPoint.DisplayTest(() -> NetworkConstants.IGNORESERVERONLY, (a, b) -> true));
+    }
+}
---
+++ fabric-particles-v1/src/testmod/java/net/fabricmc/fabric/test/particle/client/ParticleTestModClient.java
@@ -0,0 +1,16 @@
+package net.fabricmc.fabric.test.particle.client;
+
+import net.fabricmc.fabric.api.client.particle.v1.ParticleFactoryRegistry;
+import net.fabricmc.fabric.test.particle.ParticleTestMod;
+import net.minecraft.client.particle.FlameParticle;
+
+public class ParticleTestModClient {
+
+    public static void onInitializeClient() {
+        /* Registers our particle client-side.
+         * First argument is our particle's instance, created previously on ExampleMod.
+         * Second argument is the particle's factory. The factory controls how the particle behaves.
+         * In this example, we'll use FlameParticle's Factory. */
+        ParticleFactoryRegistry.getInstance().register(ParticleTestMod.GREEN_FLAME, FlameParticle.Factory::new);
+    }
+}
