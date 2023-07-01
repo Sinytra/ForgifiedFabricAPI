61bd57698f0dd37fc86f5b3c03b83f0df4a7f3ed
Su5eD <su5ed@outlook.com>
2023-07-01 13:52:42 +0200

    Port fabric-rendering-data-attachment-v1

---
+++ fabric-rendering-data-attachment-v1/src/main/java/net/fabricmc/fabric/impl/rendering/data/attachment/RenderingDataAttachmentImpl.java
@@ -0,0 +1,9 @@
+package net.fabricmc.fabric.impl.rendering.data.attachment;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod("fabric_rendering_data_attachment_v1")
+public class RenderingDataAttachmentImpl {
+    public RenderingDataAttachmentImpl() {
+    }
+}
