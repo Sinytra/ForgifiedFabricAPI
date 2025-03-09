58235da0d768450e4e07d2b97bad889a8fe7b7aa
Octol1ttle <l1ttleofficial@outlook.com>
2025-03-09 14:58:08 +0100

    fix(attsync): use correct event for transferring attachments during respawn (#4483)
    (cherry picked from commit e99da0f77af5ed62f70c88ecee3f98815aaf3555)
    
    # Conflicts:
    #	fabric-data-attachment-api-v1/src/main/java/net/fabricmc/fabric/impl/attachment/AttachmentEntrypoint.java

--- fabric-data-attachment-api-v1/src/main/java/net/fabricmc/fabric/impl/attachment/AttachmentEntrypoint.java
+++ fabric-data-attachment-api-v1/src/main/java/net/fabricmc/fabric/impl/attachment/AttachmentEntrypoint.java
@@ -34,7 +34,7 @@ public class AttachmentEntrypoint implements ModInitializer {
 
     @Override
     public void onInitialize() {
-        ServerPlayerEvents.COPY_FROM.register((oldPlayer, newPlayer, alive) ->
+        ServerPlayerEvents.AFTER_RESPAWN.register((oldPlayer, newPlayer, alive) ->
             transfer(oldPlayer, newPlayer, true, !alive)
         );
         ServerEntityWorldChangeEvents.AFTER_ENTITY_CHANGE_WORLD.register((originalEntity, newEntity, origin, destination) ->
