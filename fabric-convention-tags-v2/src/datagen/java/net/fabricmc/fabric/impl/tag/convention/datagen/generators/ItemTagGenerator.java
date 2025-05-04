TexBlock/FAPI-Mojmaps

[1.21.1] Backport Convention Drink Tags (#4537)
* Convention Drink Tags (#4384)

* add drink tag keys

* add tags to datagen

* generate tags

* add translations

* drink container tags

* update wording of javadoc

* fix grammar in javadoc

* fix more javadoc typos

* add juice drink type tags

* number the rules for a drink

* watery tag; clarify rule 4 of drink definition

* remove rule 4 of the drinks tag

* fix java doc typo

* generate watery tag

* match juice tag names to vanilla ids

* remove subsub juice tags

* fix potential inconsistency in the drinks javadoc for alcoholic drinks

* remove sub sub tags

(cherry picked from commit c81f59020fb233f9701a85a0a592e561f7e9e458)

* correct drink use action javadoc link

fabric-convention-tags-v2/src/datagen/java/net/fabricmc/fabric/impl/tag/convention/datagen/generators/ItemTagGenerator.java
@@ -373,6 +373,44 @@ public final class ItemTagGenerator extends FabricTagProvider.ItemTagProvider {
 				.addOptionalTag(ConventionalItemTags.EDIBLE_WHEN_PLACED_FOODS)
 				.addOptionalTag(ConventionalItemTags.FOOD_POISONING_FOODS);
 
+		getOrCreateTagBuilder(ConventionalItemTags.DRINKS)
+				.addOptionalTag(ConventionalItemTags.WATER_DRINKS)
+				.addOptionalTag(ConventionalItemTags.WATERY_DRINKS)
+				.addOptionalTag(ConventionalItemTags.MILK_DRINKS)
+				.addOptionalTag(ConventionalItemTags.HONEY_DRINKS)
+				.addOptionalTag(ConventionalItemTags.MAGIC_DRINKS)
+				.addOptionalTag(ConventionalItemTags.OMINOUS_DRINKS)
+				.addOptionalTag(ConventionalItemTags.JUICE_DRINKS);
+
+		getOrCreateTagBuilder(ConventionalItemTags.WATER_DRINKS);
+
+		getOrCreateTagBuilder(ConventionalItemTags.WATERY_DRINKS)
+				.add(Items.POTION)
+				.addOptionalTag(ConventionalItemTags.WATER_DRINKS);
+
+		getOrCreateTagBuilder(ConventionalItemTags.MILK_DRINKS)
+				.add(Items.MILK_BUCKET);
+
+		getOrCreateTagBuilder(ConventionalItemTags.HONEY_DRINKS)
+				.add(Items.HONEY_BOTTLE);
+
+		getOrCreateTagBuilder(ConventionalItemTags.MAGIC_DRINKS)
+				.add(Items.POTION)
+				.addOptionalTag(ConventionalItemTags.OMINOUS_DRINKS);
+
+		getOrCreateTagBuilder(ConventionalItemTags.OMINOUS_DRINKS)
+				.add(Items.OMINOUS_BOTTLE);
+
+		getOrCreateTagBuilder(ConventionalItemTags.JUICE_DRINKS);
+
+		getOrCreateTagBuilder(ConventionalItemTags.DRINK_CONTAINING_BUCKET)
+				.add(Items.MILK_BUCKET);
+
+		getOrCreateTagBuilder(ConventionalItemTags.DRINK_CONTAINING_BOTTLE)
+				.add(Items.POTION)
+				.add(Items.HONEY_BOTTLE)
+				.add(Items.OMINOUS_BOTTLE);
+
 		// Deprecated tags below
 		getOrCreateTagBuilder(ConventionalItemTags.FRUITS_FOODS)
 				.add(Items.APPLE)

Sinytra/ForgifiedFabricAPI

fabric-convention-tags-v2/src/datagen/java/net/fabricmc/fabric/impl/tag/convention/datagen/generators/ItemTagGenerator.java
@@ -371,6 +371,44 @@ public final class ItemTagGenerator extends FabricTagProvider.ItemTagProvider {
 				.addOptionalTag(ConventionalItemTags.EDIBLE_WHEN_PLACED_FOODS)
 				.addOptionalTag(ConventionalItemTags.FOOD_POISONING_FOODS);
 
+		tag(ConventionalItemTags.DRINKS)
+				.addOptionalTag(ConventionalItemTags.WATER_DRINKS)
+				.addOptionalTag(ConventionalItemTags.WATERY_DRINKS)
+				.addOptionalTag(ConventionalItemTags.MILK_DRINKS)
+				.addOptionalTag(ConventionalItemTags.HONEY_DRINKS)
+				.addOptionalTag(ConventionalItemTags.MAGIC_DRINKS)
+				.addOptionalTag(ConventionalItemTags.OMINOUS_DRINKS)
+				.addOptionalTag(ConventionalItemTags.JUICE_DRINKS);
+
+		tag(ConventionalItemTags.WATER_DRINKS);
+
+		tag(ConventionalItemTags.WATERY_DRINKS)
+				.add(Items.POTION)
+				.addOptionalTag(ConventionalItemTags.WATER_DRINKS);
+
+		tag(ConventionalItemTags.MILK_DRINKS)
+				.add(Items.MILK_BUCKET);
+
+		tag(ConventionalItemTags.HONEY_DRINKS)
+				.add(Items.HONEY_BOTTLE);
+
+		tag(ConventionalItemTags.MAGIC_DRINKS)
+				.add(Items.POTION)
+				.addOptionalTag(ConventionalItemTags.OMINOUS_DRINKS);
+
+		tag(ConventionalItemTags.OMINOUS_DRINKS)
+				.add(Items.OMINOUS_BOTTLE);
+
+		tag(ConventionalItemTags.JUICE_DRINKS);
+
+		tag(ConventionalItemTags.DRINK_CONTAINING_BUCKET)
+				.add(Items.MILK_BUCKET);
+
+		tag(ConventionalItemTags.DRINK_CONTAINING_BOTTLE)
+				.add(Items.POTION)
+				.add(Items.HONEY_BOTTLE)
+				.add(Items.OMINOUS_BOTTLE);
+
 		// Deprecated tags below
 		tag(ConventionalItemTags.FRUITS_FOODS)
 				.add(Items.APPLE)